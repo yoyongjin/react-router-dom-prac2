6df28682e17b8065d78a9699fe1f8daf09a94ba9
yoyongjin <yongjinalize@gmail.com>
2022-06-19 19:11:05 +0900

    NotFound page added

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,15 +1,25 @@
 import React, { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Yongjin", text: "I am Yongjin" },
+  { id: "q2", author: "Eunbin", text: "I am Eunbin" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <Fragment>
-      <h1>QuoteDetail</h1>
-      <p>{params.quoteId}</p>
-
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
