4995fd993024f5029c94fd4061595841b39cfd27
yoyongjin <yongjinalize@gmail.com>
2022-06-28 18:08:51 +0900

    Asc/Desc btn using useHistory & useLocation

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,44 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  // JS 기본 생성자 함수, 기본 class. 브라우저에서 이용.
+  const queryParams = new URLSearchParams(location.search);
+
+  const isSortAscending = queryParams.get("sort") === "asc";
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortAscending);
+
+  const changeSortHandler = () => {
+    history.push("/quotes?sort=" + (isSortAscending ? "desc" : "asc"));
+    // queryParams에 {sort: 'asc'}의 키-값 쌍이 추가된다.
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortHandler}>
+          Sort {isSortAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
